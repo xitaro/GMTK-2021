44ced7e2cbef074f1ecbbc762f642cf9427b8b7b
xitaro <marcobsaraiva@gmail.com>
2021-06-12 11:29:22 -0300

    Empty project + Ground rotating

---
+++ GMTK 2021/Assets/Ground.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    // The speed the planet will rotate
+    [SerializeField] private float speed;    
+
+    private void Update()
+    {
+        // Spin the object around the target at (speed) degrees/second.
+        transform.RotateAround(transform.position, Vector3.left, speed * Time.deltaTime);
+    }
+}
