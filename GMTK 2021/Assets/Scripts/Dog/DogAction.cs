ab1bc664400cc64364a0c623da68d4f65f369f47
xitaro <marcobsaraiva@gmail.com>
2021-06-12 18:29:37 -0300

    Dog Actions
    Prefab dos itens e obstáculos tem que ter o script "Interactable";
    "Interactable" tem um unity event, que deve ser colocado diretamente no inspector, (referenciar Dog Prefab e escolher ação);
    Dog tem o script "DogActions" onde cada ação dele é uma função; Exemplo: Bark;

---
+++ GMTK 2021/Assets/Scripts/Dog/DogAction.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class DogAction : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Interactable interactable = other.GetComponent<Interactable>(); 
+        if (interactable != null)
+        {
+            interactable.action.Invoke();
+        }
+    }
+
+    public void Bark()
+    {
+        Debug.Log("Bark");
+    }
+
+    public void Pee()
+    {
+        Debug.Log("Pee");
+    }
+
+    public void Poop()
+    {
+        Debug.Log("Poop");
+    }
+
+    public void PokeAround()
+    {
+        Debug.Log("Poke");
+    }
+
+    public void DrinkWater()
+    {
+        Debug.Log("Drink");
+    }
+}
---
+++ GMTK 2021/Assets/Scripts/Items/Interactable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Interactable : MonoBehaviour
+{
+    public string type;
+
+    // Action to the dog to perform
+    public UnityEvent action;
+
+}
